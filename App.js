2301250184bd221066e9d15b29ba6526f910ef21
apple <siyabarshadsatti@gmail.com>
2022-09-04 13:49:58 +0500

    ready

---
+++ App.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from "expo-font"
+import * as SplashScreen from 'expo-splash-screen';
+export default function App() {
+  const [fontsLoaded] =useFonts({
+      'Rubik-Bold':require("./assets/fonts/Rubik-Bold.ttf"),
+      'Rubik-ExtraBold':require("./assets/fonts/Rubik-ExtraBold.ttf"),
+      'Rubik-Light':require("./assets/fonts/Rubik-Light.ttf"),
+      'Rubik-Medium':require("./assets/fonts/Rubik-Medium.ttf"),
+      'Rubik-Regular':require("./assets/fonts/Rubik-Regular.ttf"),
+      'Rubik-SemiBold':require("./assets/fonts/Rubik-SemiBold.ttf"),
+      'VarelaRound-Regular':require("./assets/fonts/VarelaRound-Regular.ttf")
+  })
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+} useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }    
+    prepare();
+    setTimeout(async() => {
+      await SplashScreen.hideAsync()
+    }, 2000);
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/extra/colors.js
@@ -0,0 +1,15 @@
+const colors={
+    primary:"#FF4B3A",
+    secondary:"#F2F2F2",
+    grey1:"#F6F6F9",
+    grey2:"#9A9A9D",
+    grey3:"#EFEEEE",
+    grey4:"#C4C4C4",
+    grey5:"#EDEDED",
+    white:"#FFFFFF",
+    black:"#000000",
+    blue:"#0038FF",
+    pink:"#EB4796",
+    orange:"#F47B0A",
+
+}
